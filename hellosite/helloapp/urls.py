69c2a4032e8d492f0a02761c1ab15dd8c3bac513
Allison Kroboth <42.allison.k@gmail.com>
2022-04-03 16:02:11 -0400

    corrected hellosite

--- hellosite/helloapp/urls.py
+++ hellosite/helloapp/urls.py
@@ -1,7 +1,8 @@
-# from django.urls import path
-# from helloapp.views import HelloWorldView, 
+from django.urls import path
+from helloapp.views import HelloWorldView, HelloNameView, GoodbyeView
 
-# urlpatterns = [
-#     path('', HelloWorldView.as_view(), name='hello_world'),
-    
-#     ]
+urlpatterns = [
+    path('', HelloWorldView.as_view(), name='hello_world'),
+    path('<name>', HelloNameView.as_view(), name='hello_name'),
+    path('goodbye/<name>', GoodbyeView.as_view(), name='goodbye'),
+    ]
--- hellosite/hellosite/urls.py
+++ hellosite/hellosite/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from helloapp.views import HelloWorldView, HelloNameView, GoodbyeView
+# from helloapp.views import HelloWorldView, HelloNameView, GoodbyeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HelloWorldView.as_view(), name='hello_world'),
-    path('hello/<name>', HelloNameView.as_view(), name='hello_name'),
-    path('hello/goodbye/<name>', GoodbyeView.as_view(), name='goodbye')
-    
+    path('hello/', include('helloapp.urls'))
     ]
